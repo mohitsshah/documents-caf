9a710ead6a2eefa51e49a2f96df182718516d8d0
Mohit <mohit@Sandeeps-MacBook-Air.local>
2018-04-24 12:02:37 +0530

    tweaks

--- Information-Extraction/arrange_blocks.py
+++ Information-Extraction/arrange_blocks.py
@@ -19,6 +19,13 @@ class Reader(object):
             obj[item[0]] = item[1]
         return obj
 
+    def is_y_similar(self, ry0, ry1, y0, y1):
+        if ry0 == y0:
+            return True
+        if ry0 < y0 < ry1:
+            return True
+        return False
+
     def get_page_text(self, tree, dims):
         width = dims[0]
         height = dims[1]
@@ -49,6 +56,65 @@ class Reader(object):
                         lines.append(line_coords)
                         text.append(words)
                         line_num += 1
+        figure_boxes = tree.findall("figure")
+        fig_words = []
+        unique_y = {}
+        for box in figure_boxes:
+            for item in box:
+                if item.tag == "text":
+                    word_coords = item.attrib["bbox"].split(",")
+                    word_coords = [float(b) for b in word_coords]
+                    unique_y[(word_coords[1], word_coords[3])] = True
+                    word_coords.append(item.text)
+                    fig_words.append(word_coords)
+        fig_words = sorted(fig_words, key=lambda x: (-x[1], x[0]))
+        fig_line = []
+        fig_inds = []
+        for y in unique_y.keys():
+            ref_y0 = y[0]
+            ref_y1 = y[1]
+            W = []
+            tmp_inds = []
+            for idx, w in enumerate(fig_words):
+                if idx not in fig_inds:
+                    y0 = w[1]
+                    y1 = w[3]
+                    if self.is_y_similar(ref_y0, ref_y1, y0, y1):
+                        W.append(w)
+                        tmp_inds.append(idx)
+            W = sorted(W, key=lambda x: (-x[1], x[0]))
+            tmp_inds = list(set(tmp_inds))
+            fig_inds.extend(tmp_inds)
+            widths = []
+            for i, ww in enumerate(W):
+                widths.append(ww[2] - ww[0])
+            med = np.mean(widths) if len(widths) > 0 else 0.
+            line = []
+            for i, ww in enumerate(W):
+                if i == 0:
+                    line.append(ww)
+                else:
+                    g = ww[0] - W[i-1][2]
+                    if g > 4.*med:
+                        chars = []
+                        for l in line:
+                            chars.append(l[-1])
+                        if len(chars) > 0:
+                            bb = [line[0][0], line[0][1], line[-1][2], line[-1][3], line_num, line_num, "".join(chars)]
+                            fig_line.append(bb)
+                        line = [ww]
+                    else:
+                        line.append(ww)
+            chars = []
+            for l in line:
+                chars.append(l[-1])
+            if len(chars) > 0:
+                bb = [line[0][0], line[0][1], line[-1][2], line[-1][3], line_num, line_num, "".join(chars)]
+                fig_line.append(bb)
+            line_num += 1
+            unique_y[y] = W
+
+        lines.extend(fig_line)
         groups = {}
         indices = []
         lines = sorted(lines, key=lambda x: (-x[1], x[0]))
@@ -57,14 +123,16 @@ class Reader(object):
             idx = line[-2]
             if idx not in indices:
                 tmp = [idx]
-                ref_y = line[1]
+                ref_y0 = line[1]
+                ref_y1 = line[3]
                 end_x = line[2]
                 for line2 in lines:
                     idx2 = line2[-2]
                     if (idx2 not in indices) and (idx != idx2):
-                        y = line2[1]
+                        y0 = line2[1]
+                        y1 = line2[3]
                         x = line2[0]
-                        if y == ref_y:
+                        if self.is_y_similar(ref_y0, ref_y1, y0, y1):
                             if np.abs(x - end_x) > 0.4 * width:
                                 pass
                             else:
@@ -77,11 +145,19 @@ class Reader(object):
                 indices.extend(tmp)
                 indices = list(set(indices))
         new_groups = {}
+        block_types = {}
         indices = []
+        block_type = 0
         keys = sorted(groups, key=lambda k: len(groups[k]), reverse=True)
         for k in keys:
             inds = []
             items = groups[k]
+            if len(items) < 2:
+                block_type = 0
+            elif len(items) > 2:
+                block_type = 2
+            else:
+                block_type = 1
             for item in items:
                 idx = item[-2]
                 if idx not in indices:
@@ -104,18 +180,44 @@ class Reader(object):
             tmp_lines = sorted(tmp_lines, key=lambda x: (-x[1], x[0]))
             if len(tmp_lines) > 0:
                 new_groups[tmp_lines[0][-2]] = tmp_lines
+                block_types[tmp_lines[0][-2]] = block_type
             indices = list(set(indices))
         blocks = []
+        key_values = []
+        multi_columns = []
         for k, v in new_groups.items():
-            tmp = []
-            for item in v:
-                tmp.append(item[-1].rstrip().lstrip())
-            text = " ".join(tmp)
-            if len(text.rstrip().lstrip()) > 0:
-                blocks.append([v[0][0], v[0][1], v[-1][2], v[-1][3], text])
+            if block_types[k] == 0:
+                tmp = []
+                for item in v:
+                    tmp.append(item[-1].rstrip().lstrip())
+                text = " ".join(tmp)
+                if len(text.rstrip().lstrip()) > 0:
+                    blocks.append([v[0][0], v[0][1], v[-1][2], v[-1][3], text])
+            elif block_types[k] == 1:
+                ref_item = v[0]
+                ref_x = ref_item[2]
+                K = [ref_item]
+                V = []
+                for item in v[1:]:
+                    x = item[0]
+                    if x > ref_x:
+                        V.append(item)
+                    else:
+                        K.append(item)
+                k_text = []
+                v_text = []
+                for kk in K:
+                    k_text.append(kk[-1])
+                for kk in V:
+                    v_text.append(kk[-1])
+                key_values.append([" ".join(k_text), " ".join(v_text)])
+            elif block_types[k] == 2:
+                print (v)
+                pass
+                # print (v)
         blocks = sorted(blocks, key=lambda x: (-x[1], x[0]))
         blocks = [b[-1] for b in blocks]
-        return blocks
+        return blocks, key_values
 
     def search_phrase(self, phrase, blocks):
         matches = []
@@ -168,12 +270,14 @@ class Reader(object):
 
         print('Number of Pages: ', len(page_ids))
         blocks = []
+        key_values = []
         for i, id in enumerate(page_ids):
             selector = "./page[@id=" + "'" + id + "']"
             page_tree = root.find(selector)
-            page_blocks = self.get_page_text(page_tree, page_dims[i])
+            page_blocks, kv = self.get_page_text(page_tree, page_dims[i])
             blocks.append(page_blocks)
-        return blocks
+            key_values.extend(kv)
+        return blocks, key_values
 
 
 if __name__ == '__main__':
