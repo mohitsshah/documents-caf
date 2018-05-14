d1ea46e82b3dae7b3b5d1ed1b19048d535955963
Mohit <mohit@Sandeeps-MacBook-Air.local>
2018-05-14 12:11:46 +0530

    changes

---
+++ Document-Processor/extract_tiff.py
@@ -0,0 +1,67 @@
+import os
+import argparse
+import shutil
+from modules.tiffProcessor import Reader
+import glob
+
+
+def get_files(src):
+    files = []
+    for root, directories, filenames in os.walk(src):
+        for f in filenames:
+            if f.endswith(".tiff") or f.endswith(".tif"):
+                files.append((f.split(".")[0], f.split(".")[1], root.replace(src, "").lstrip("/"), os.path.join(root, f)))
+    return files
+
+
+def process_file(item, dst, overwrite, tessdata, tessdata3):
+    filename, ext, dirs, src = item
+    dst_dir = os.path.join(dst, dirs, filename)
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir, exist_ok=True)
+    dst_file = os.path.join(dst_dir, filename + "." + ext)
+    if os.path.exists(os.path.join(dst_dir, filename + ".json")) and not overwrite:
+        return False
+    shutil.copy(src, dst_file)
+    reader = Reader(
+        params={"src": dst_dir, "name": filename, "overwrite": overwrite, "tessdata": tessdata, "tessdata3": tessdata3, "ext": ext})
+    status, message = reader.extract()
+    if not status:
+        print(message)
+    return status
+
+
+def run(args):
+    src = args.src
+    if not os.path.exists(src):
+        raise Exception("Source directory %s does not exist" % src)
+    dst = args.dst
+    if not os.path.exists(dst):
+        os.makedirs(dst, exist_ok=True)
+    overwrite = args.overwrite
+    overwrite = True if overwrite == "y" else False
+    tessdata = args.tessdata
+    tessdata = tessdata if len(tessdata) > 0 else None
+    tessdata3 = args.tessdata3
+    tessdata3 = tessdata3 if len(tessdata3) > 0 else None
+    files = get_files(src)
+    print("Found %d TIFF files in %s" % (len(files), src))
+    for f in files:
+        print("Processing %s" % f[-1], "-In Progress")
+        status = process_file(f, dst, overwrite, tessdata, tessdata3)
+        if status:
+            print("Processing %s" % f[-1], "-Complete")
+
+
+if __name__ == '__main__':
+    flags = argparse.ArgumentParser("Command line arguments for TIFF Document Processing")
+    flags.add_argument("-src", type=str, required=True, help="Source directory of files")
+    flags.add_argument("-dst", type=str, required=True, help="Destination directory")
+    flags.add_argument("-overwrite", type=str, choices=["y", "n"], default="n", help="Overwrite files")
+    flags.add_argument("-tessdata", type=str, default="", help="Path to Tessdata model (v4) for tesserocr")
+    flags.add_argument("-tessdata3", type=str, default="", help="Path to Tessdata model (v3) for osd")
+    args = flags.parse_args()
+    try:
+        run(args)
+    except Exception as e:
+        print(str(e))
---
+++ Document-Processor/modules/tiffProcessor.py
@@ -0,0 +1,184 @@
+import os
+import json
+import argparse
+import re
+import numpy as np
+from tesserocr import PyTessBaseAPI, RIL, PSM, iterate_level, OEM
+
+BlockType = [
+    "UNKNOWN",
+    "FLOWING_TEXT",
+    "HEADING_TEXT",
+    "PULLOUT_TEXT",
+    "EQUATION",
+    "INLINE_EQUATION",
+    "TABLE",
+    "VERTICAL_TEXT",
+    "CAPTION_TEXT",
+    "FLOWING_IMAGE",
+    "HEADING_IMAGE",
+    "PULLOUT_IMAGE",
+    "HORZ_LINE",
+    "VERT_LINE",
+    "NOISE",
+    "COUNT"
+]
+
+
+class PageOCR(object):
+    def __init__(self, image_file, tessdata):
+        api = PyTessBaseAPI(path=tessdata, psm=PSM.AUTO_OSD)
+        api.SetImageFile(image_file)
+        api.SetVariable("textord_tablefind_recognize_tables", "T")
+        api.SetVariable("textord_tabfind_find_tables", "T")
+        api.Recognize()
+        self.api = api
+
+    def get_page(self):
+        api = self.api
+        document = None
+        ri = api.GetIterator()
+        if ri is not None:
+            document = {"blocks": []}
+            while ri.IsAtBeginningOf(RIL.BLOCK):
+                block = {"block_type": ri.BlockType(), "block_type_str": BlockType[ri.BlockType()],
+                         "box": ri.BoundingBox(RIL.BLOCK), "ocr_text": ri.GetUTF8Text(RIL.BLOCK),
+                         "confidence": ri.Confidence(RIL.BLOCK), "paragraphs": []}
+                break_para = False
+                while True:
+                    if ri.IsAtFinalElement(RIL.BLOCK, RIL.PARA):
+                        break_para = True
+                    break_line = False
+                    paragraph = {"box": ri.BoundingBox(RIL.PARA), "ocr_text": ri.GetUTF8Text(RIL.PARA),
+                                 "paragraph_info": list(ri.ParagraphInfo()),
+                                 "confidence": ri.Confidence(RIL.PARA), "lines": []}
+                    while True:
+                        if ri.IsAtFinalElement(RIL.PARA, RIL.TEXTLINE):
+                            break_line = True
+                        break_word = False
+                        line = {"box": ri.BoundingBox(RIL.TEXTLINE), "ocr_text": ri.GetUTF8Text(RIL.TEXTLINE),
+                                "confidence": ri.Confidence(RIL.TEXTLINE), "words": []}
+                        while True:
+                            word = {"box": ri.BoundingBox(RIL.WORD), "ocr_text": ri.GetUTF8Text(RIL.WORD),
+                                    "confidence": ri.Confidence(RIL.WORD), "attributes": ri.WordFontAttributes()}
+                            if ri.IsAtFinalElement(RIL.TEXTLINE, RIL.WORD):
+                                break_word = True
+                            line["words"].append(word)
+                            if break_word:
+                                break
+                            ri.Next(RIL.WORD)
+                        paragraph["lines"].append(line)
+                        if break_line:
+                            break
+                        ri.Next(RIL.TEXTLINE)
+                    block["paragraphs"].append(paragraph)
+                    if break_para:
+                        break
+                    ri.Next(RIL.PARA)
+                document["blocks"].append(block)
+                ri.Next(RIL.BLOCK)
+        return document
+
+
+class Reader(object):
+    def __init__(self, args=None, params=None):
+        if params is not None:
+            self.file_path = params["src"]
+            self.file_name = params["name"]
+            self.overwrite = params["overwrite"]
+            self.tessdata = params["tessdata"]
+            self.tessdata3 = params["tessdata3"]
+            self.ext = params["ext"]
+        else:
+            self.file_path = args.src
+            self.file_name = args.name
+            self.overwrite = args.overwrite
+            self.tessdata = args.tessdata
+            self.tessdata3 = args.tessdata3
+
+    def read_ocr_document(self, doc):
+        words = []
+        for block in doc["blocks"]:
+            for para in block["paragraphs"]:
+                for line in para["lines"]:
+                    for word in line["words"]:
+                        tmp = word["box"]
+                        tmp = [float(t) for t in tmp]
+                        w = word["ocr_text"].rstrip().lstrip()
+                        if len(w) > 0:
+                            tmp.append(word["ocr_text"])
+                            words.append(tmp)
+        words = sorted(words, key=lambda x: (x[1], x[0]))
+        return words
+
+    def get_orientation(self, image_file):
+        or_file = os.path.join(self.file_path, "orientation")
+        cmd = "tesseract --psm 0 " + image_file + " " + or_file
+        os.system(cmd)
+        or_text = open(or_file).read()
+        return or_text
+
+    def rotate_image(self, image_file, orientation):
+        rotation = int(360 - orientation)
+        cmd = "convert -density 300 -units PixelsPerInch -rotate %d %s %s" % (
+            rotation, image_file, image_file)
+        os.system(cmd)
+        return image_file
+
+    def get_page_text(self, image_file):
+        # osd = self.get_orientation(image_file)
+        # orientation = osd["orient_deg"]
+        # if orientation != 0:
+        #     image_file = self.rotate_image(image_file, orientation)
+        ocr_api = PageOCR(image_file, self.tessdata)
+        doc = ocr_api.get_page()
+        words = []
+        if doc is not None:
+            words.extend(self.read_ocr_document(doc))
+        words = sorted(words, key=lambda x: (x[1], x[0]))
+        return doc, words
+
+    def convert_to_png(self, name, infile, image_dir, overwrite=False, dpi=300):
+        if os.path.exists(image_dir) and not overwrite:
+            files = os.listdir(image_dir)
+            files = [os.path.join(image_dir, f) for f in files]
+            return files
+        os.makedirs(image_dir, exist_ok=True)
+        outfile = os.path.join(image_dir, name + ".png")
+        cmd = "convert -density %s -units PixelsPerInch %s %s" % (
+            int(dpi), infile, outfile)
+        os.system(cmd)
+        files = os.listdir(image_dir)
+        files = [os.path.join(image_dir, f) for f in files]
+        return files
+
+    def extract(self):
+        tiff_path = os.path.join(
+            self.file_path, self.file_name + "." + self.ext)
+        image_dir = os.path.join(self.file_path, "images")
+        images = self.convert_to_png(
+            self.file_name, tiff_path, image_dir, self.overwrite)
+        print('Number of Pages: ', len(images))
+        document = {"name": self.file_name, "path": self.file_path,
+                    "total_pages": len(images), "pages": []}
+        for num, image_path in enumerate(images, 1):
+            doc, words = self.get_page_text(image_path)
+            page_json_file = self.file_name + "-%d.json" % num
+            page_json_path = os.path.join(self.file_path, page_json_file)
+            with open(page_json_path, "w") as fi:
+                fi.write(json.dumps(doc))
+            document["pages"].append({"words": words})
+        with open(os.path.join(self.file_path, self.file_name + ".json"), "w") as fi:
+            fi.write(json.dumps(document))
+        return True, None
+
+
+if __name__ == '__main__':
+    flags = argparse.ArgumentParser(
+        "Command line arguments for Document Processing")
+    flags.add_argument("-src", type=str, required=True,
+                       help="Source file path")
+    flags.add_argument("-name", type=str, required=True, help="Name of file")
+    args = flags.parse_args()
+    reader = Reader(args=args)
+    reader.extract()
